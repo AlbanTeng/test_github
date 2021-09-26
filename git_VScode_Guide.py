bd5c110bc06d4be71b55d0ff17d7da468a81a913
AlbanTeng <jialeteng1984@163.com>
2021-09-26 16:09:33 +0800

    name change

--- git_VScode_Guide.py
+++ git_VScode_Guide.py
@@ -3,11 +3,11 @@
 # Git Add remote URL and repo name
     # Click View -> enter Git Add Remote
     # Enter Git repo url via login github website and create a repo that is as same name as local repo(/project)
-# in Source control side bar(third one in common), in Message box, enter init to initalize the one.
+# in Source control side bar(third one in common), in Message box, enter init to initalize the one and and click Check button.
     # Click Push button to upload local data on github
 
 #modify local data and then upload the change
-    # Click Source control side bar, in the Message box, enter "name change"(which means commit the changes)
+    # Click Source control side bar, in the Message box, enter "name change"(which means commit the changes) and click Check button.
     # Click push button to upload the change on github  
 
 #for more details, please check the video https://www.youtube.com/watch?v=3Tn58KQvWtU
